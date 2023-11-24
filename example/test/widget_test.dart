b6df2a4cbd8d83d4002caf2a12d1f3c72e684fc8
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2023-11-24 15:58:12 +0000

    Add trailing comma

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,24 +11,26 @@ import 'package:haptic_feedback/haptic_feedback.dart';
 import 'package:haptic_feedback_example/main.dart';
 
 void main() {
-  testWidgets('Haptics types are displayed in example',
-      (WidgetTester tester) async {
-    // Set the size of the testing window.
-    tester.view.physicalSize = const Size(1080, 1920);
-    tester.view.devicePixelRatio = 1;
+  testWidgets(
+    'Haptics types are displayed in example',
+    (WidgetTester tester) async {
+      // Set the size of the testing window.
+      tester.view.physicalSize = const Size(1080, 1920);
+      tester.view.devicePixelRatio = 1;
 
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that it shows the last haptics type.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) {
-          return widget is Text &&
-              widget.data!.startsWith(HapticsType.values.last.name);
-        },
-      ),
-      findsOneWidget,
-    );
-  });
+      // Verify that it shows the last haptics type.
+      expect(
+        find.byWidgetPredicate(
+          (Widget widget) {
+            return widget is Text &&
+                widget.data!.startsWith(HapticsType.values.last.name);
+          },
+        ),
+        findsOneWidget,
+      );
+    },
+  );
 }
