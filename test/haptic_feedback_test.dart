5946a06665fedf458c469005b0413825debbf557
Joachim Tobias Nohl <43643339+nohli@users.noreply.github.com>
2025-11-29 12:39:21 +0000

    Add test

--- test/haptic_feedback_test.dart
+++ test/haptic_feedback_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:haptic_feedback/src/haptic_feedback_method_channel.dart';
 import 'package:haptic_feedback/src/haptic_feedback_platform_interface.dart';
@@ -63,6 +64,16 @@ void main() {
     HapticFeedbackPlatform.instance = fakePlatform;
 
     expect(await Haptics.canVibrate(), true);
+  });
+
+  test('canVibrate returns false on unsupported platforms', () async {
+    debugDefaultTargetPlatformOverride = TargetPlatform.windows;
+    addTearDown(() => debugDefaultTargetPlatformOverride = null);
+
+    const fakePlatform = MockHapticFeedbackPlatform();
+    HapticFeedbackPlatform.instance = fakePlatform;
+
+    expect(await Haptics.canVibrate(), false);
   });
 
   test('vibrate forwards usage to the platform implementation', () async {
